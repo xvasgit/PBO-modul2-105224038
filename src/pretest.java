8cbfe40e927ddee228a50e453ad2483cddacca84
xvasgit <erlanggapermana8279@gmail.com>
2026-03-03 14:13:11 +0700

    commit

---
+++ src/pretest.java
@@ -0,0 +1,11 @@
+public class pretest {
+    public static void main(String[] args) throws Exception {
+        String nama = "Erlangga Aditya Permana";
+        int NIM = 105224038;
+        String prodi = "Ilmu Komputer";
+        System.out.println("==========================");
+        System.out.println("KARTU TANDA MAHASISWA");
+        System.out.println("Nama\t: " + nama + "\nNIM\t: " + NIM + "\nProdi\t: " + prodi );
+        System.out.println("==========================");
+    }
+}
